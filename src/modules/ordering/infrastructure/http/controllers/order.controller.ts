109e7cc4c9c327dfb1b585da08515362f3408a7d
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2026-03-07 10:16:57 +0100

    Refactored place order command

--- src/modules/ordering/infrastructure/http/controllers/order.controller.ts
+++ src/modules/ordering/infrastructure/http/controllers/order.controller.ts
@@ -2,13 +2,13 @@ import type { Request, Response } from 'express';
 import { placeOrderDtoSchema } from '../dtos/place-order.dto.ts';
 import { randomUUID } from 'node:crypto';
 import type { PlaceOrderHandler } from '../../../application/command-handlers/place-order.handler.ts';
+import type { PlaceOrderCommand } from '../../../application/commands/place-order.command.ts';
 
 export class OrderController {
   constructor(private readonly placeOrderHandler: PlaceOrderHandler) {}
 
   async create(request: Request, response: Response) {
     const parsedBody = placeOrderDtoSchema.safeParse(request.body);
-    const orderId = randomUUID();
 
     if (!parsedBody.success) {
       return response.status(400).json({
@@ -17,15 +17,17 @@ export class OrderController {
       });
     }
 
+    const placeOrderCommand: PlaceOrderCommand = {
+      orderId: randomUUID(),
+      customerId: parsedBody.data.customerId,
+      items: parsedBody.data.items,
+    };
+
     try {
-      await this.placeOrderHandler.execute({
-        orderId,
-        customerId: parsedBody.data.customerId,
-        items: parsedBody.data.items,
-      });
+      await this.placeOrderHandler.execute(placeOrderCommand);
 
       return response.status(201).json({
-        orderId,
+        orderId: placeOrderCommand.orderId,
         status: 'AWAITING_PAYMENT',
       });
     } catch (error: any) {
